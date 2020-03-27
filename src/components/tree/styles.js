5de64835aa60e0200ed77ec71d8e199167d31b93
Kristian Gunder Kramås <36860992+sventhevenn@users.noreply.github.com>
2020-03-27 11:06:24 +0100

    Lighter FileView lines and removed duplicate labels

--- src/components/tree/styles.js
+++ src/components/tree/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { animated } from "react-spring";
 
-const Frame = styled("div")`
+export const Frame = styled("div")`
   position: relative;
   padding: 4px 0px 0px 0px;
   text-overflow: ellipsis;
@@ -12,11 +12,11 @@ const Frame = styled("div")`
   /* fill: lightgray; */
 `;
 
-const Title = styled("span")`
+export const Title = styled("span")`
   vertical-align: middle;
 `;
 
-const Content = styled(animated.div)`
+export const Content = styled(animated.div)`
   will-change: transform, opacity, height;
   margin-left: 6px;
   padding: 0px 0px 0px 14px;
@@ -24,49 +24,10 @@ const Content = styled(animated.div)`
   overflow: hidden;
 `;
 
-const toggle = {
+export const toggle = {
   width: "1em",
   height: "1em",
   marginRight: 10,
   cursor: "pointer",
   verticalAlign: "middle"
 };
-
-export { Frame, Content, toggle, Title };
-
-// const Global = createGlobalStyle`
-//   * {
-//     box-sizing: border-box;
-//   }
-//   html,
-//   body,
-//   #root {
-//     width: 100%;
-//     height: 100%;
-//     margin: 0;
-//     padding: 0;
-//     background: #191b21;
-//     overflow: hidden;
-//     font-family: 'Monospaced Number', 'Chinese Quote', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-//       'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-//     font-size: 14px;
-//     line-height: 21px;
-//   }
-//   html,
-//   body,
-//   div,
-//   a,
-//   i,
-//   button,
-//   select,
-//   option,
-//   optgroup,
-//   hr,
-//   br {
-//     user-select: none;
-//     cursor: default;
-//   }
-//   #root {
-//     padding: 30px;
-//   }
-// `
