d1ba0dcd4ade879cfe2a0101d11be1b80327ea50
Kristian Gunder Kramås <kristiankramas@outlook.com>
2020-05-28 19:45:27 +0200

    Progress Bars

--- src/components/explorer/components/Projects.js
+++ src/components/explorer/components/Projects.js
@@ -2,6 +2,9 @@ import React from "react";
 import Tree from "components/explorer/components/Tree";
 import Link from "../../design/fonts/Link";
 import ItemGrid from "components/layout/ItemGrid";
+import { Col, ProgressBar } from "react-bootstrap";
+import { camelCaseToNormal } from "functions/general";
+import { progress, displayStage } from "functions/progress";
 
 export default ({
   data,
@@ -11,6 +14,8 @@ export default ({
   headline = "Projects",
   ...props
 }) => {
+  console.log(data);
+
   return (
     <div className={props.className}>
       {headline && <h6>{headline}</h6>}
@@ -65,29 +70,96 @@ export default ({
                   defaultOpen
                   key={`project${indexProject}Description${indexDescription}`}
                   // name={description.data.geometry}
-                  name={`Description ${indexDescription + 1}`}
+                  name={`${description.data.description}`}
                 >
                   <ItemGrid>
                     {props.access &&
                       (props.access.itemRead || props.access.itemWrite) &&
                       description.items &&
                       description.items.map((item, indexItem) => (
-                        <Link
-                          to={`/single-item/${item.id}/${description.data.geometry}`}
-                          key={`project${indexProject}Description${indexDescription}Item${indexItem}`}
-                          iconProps={{
-                            icon: ["fad", "cube"],
-                            size: iconSize,
-                            style: iconStyle
-                          }}
-                          className="mr-5"
-                          style={{ rowStyle, iconStyle }}
+                        <Col
+                          xs="12"
+                          md="6"
+                          lg="4"
+                          className="text-truncate pr-5 mb-4"
                         >
-                          {item.id}
-                        </Link>
+                          <Link
+                            to={`/single-item/${item.id}/${description.data.geometry}`}
+                            key={`project${indexProject}Description${indexDescription}Item${indexItem}`}
+                            iconProps={{
+                              icon: ["fad", "cube"],
+                              size: iconSize,
+                              style: iconStyle
+                            }}
+                            style={{ zIndex: 1, ...rowStyle }}
+                            className="text-light text-wrap text-decoration-none"
+                          >
+                            <div className="d-inline w-100">
+                              {item.itemId ? (
+                                item.itemId
+                              ) : (
+                                <div className="text-secondary d-inline">
+                                  No Item ID (Index ID: {item.id})
+                                </div>
+                              )}
+                              <ProgressBar
+                                animated
+                                variant="info"
+                                now={progress(item)}
+                                className="mt-2 shadow-sm w-100"
+                                style={{
+                                  height: "1.5em",
+                                  backgroundColor: "rgba(0, 0, 0, 0.25)"
+                                }}
+                              />
+                              <div
+                                align="center"
+                                style={{
+                                  position: "relative",
+                                  bottom: "1.4em",
+                                  height: 0,
+                                  zIndex: 0
+                                }}
+                              >
+                                <small className="text-decoration-none">
+                                  {displayStage(item)}
+                                </small>
+                              </div>
+                            </div>
+                          </Link>
+                        </Col>
                       ))}
                   </ItemGrid>
                 </Tree>
+                // <Tree
+                //   iconSize={iconSize}
+                //   iconStyle={iconStyle}
+                //   rowStyle={rowStyle}
+                //   defaultOpen
+                //   key={`project${indexProject}Description${indexDescription}`}
+                //   // name={description.data.geometry}
+                //   name={`Description ${indexDescription + 1}`}
+                // >
+                //   <ItemGrid>
+                //     {props.access &&
+                //       (props.access.itemRead || props.access.itemWrite) &&
+                //       description.items &&
+                //       description.items.map((item, indexItem) => (
+                //         <Link
+                //           to={`/single-item/${item.id}/${description.data.geometry}`}
+                //           key={`project${indexProject}Description${indexDescription}Item${indexItem}`}
+                //           iconProps={{
+                //             icon: ["fad", "cube"],
+                //             size: iconSize,
+                //             style: iconStyle
+                //           }}
+                //           style={rowStyle}
+                //         >
+                //           {item.id}
+                //         </Link>
+                //       ))}
+                //   </ItemGrid>
+                // </Tree>
               ))}
           </Tree>
         ))
--- src/components/layout/ItemGrid.js
+++ src/components/layout/ItemGrid.js
@@ -3,7 +3,7 @@ import { Row } from "react-bootstrap";
 
 export default props => {
   return (
-    <Row className="no-gutters" {...props}>
+    <Row className="no-gutters" style={props.style} {...props}>
       {props.children}
     </Row>
   );
