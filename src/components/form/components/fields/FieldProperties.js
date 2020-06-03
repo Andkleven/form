5675f4ec8eb9e2d498d1064761ccad2764bd4250
Kristian Gunder Kramås <kristiankramas@outlook.com>
2020-06-03 18:14:21 +0200

    Calculations snugs up to inputs

--- src/components/form/components/fields/FieldProperties.js
+++ src/components/form/components/fields/FieldProperties.js
@@ -15,37 +15,35 @@ import {
 } from "functions/general";
 
 export default props => {
-  const {documentDate, documentDateDispatch} = useContext(DocumentDateContext);
-  const {editChapter} = useContext(ChapterContext);
-    const getNewPath = useCallback(
-      () => {
-        if (props.type === "file") {
-          return `${props.path}.${props.fieldName}`;
-        }
-        return `${props.path ? props.path + ".data." : ""}${props.fieldName}`;
-      },
-      [props.path, props.fieldName, props.type]
-    );
-    
+  const { documentDate, documentDateDispatch } = useContext(
+    DocumentDateContext
+  );
+  const { editChapter } = useContext(ChapterContext);
+  const getNewPath = useCallback(() => {
+    if (props.type === "file") {
+      return `${props.path}.${props.fieldName}`;
+    }
+    return `${props.path ? props.path + ".data." : ""}${props.fieldName}`;
+  }, [props.path, props.fieldName, props.type]);
+
   useEffect(() => {
-    let saveState = objectPath.get(props.backendData, getNewPath(), null)
+    let saveState = objectPath.get(props.backendData, getNewPath(), null);
     if (saveState === null && !props.specValueList) {
-      let newState
+      let newState;
       if (["date", "datetime-local"].includes(props.type)) {
-        newState = emptyField(saveState) ? null : new Date(saveState)
+        newState = emptyField(saveState) ? null : new Date(saveState);
       } else {
-        newState =  props.default !== undefined
-                    ? props.default
-                    : ["checkbox", "radio", "switch"].includes(props.type)
-                    ? false
-                    : props.default === "select"
-                    ? props.options[0]
-                    : ""
+        newState =
+          props.default !== undefined
+            ? props.default
+            : ["checkbox", "radio", "switch"].includes(props.type)
+            ? false
+            : props.default === "select"
+            ? props.options[0]
+            : "";
       }
-        documentDateDispatch({type: 'add', 
-                              newState,
-                              path: getNewPath()})
-  } 
+      documentDateDispatch({ type: "add", newState, path: getNewPath() });
+    }
   }, [
     props.specValueList,
     documentDateDispatch,
@@ -55,10 +53,9 @@ export default props => {
     props.fieldName,
     getNewPath,
     props.backendData
-  ]); 
-  
-  
-  let {min, max} = calculateMaxMin(
+  ]);
+
+  let { min, max } = calculateMaxMin(
     props.min,
     props.routeToSpecMin,
     props.editRepeatStepListMin,
@@ -89,21 +86,19 @@ export default props => {
     props.subtextMathMax,
     props.repeatStepList,
     props.allData
-  )
+  );
 
-  let label = props.queryVariableLabel || props.indexVariableLabel
-        ? variableLabel(
-            props.label,
-            props.variableLabelSpec
-              ? props.specData
-              : documentDate,
-            props.queryVariableLabel,
-            props.repeatStepList,
-            props.editRepeatStepListVariableLabel,
-            props.indexVariableLabel ? props.repeatStep : undefined
-          )
-        : props.label
-
+  let label =
+    props.queryVariableLabel || props.indexVariableLabel
+      ? variableLabel(
+          props.label,
+          props.variableLabelSpec ? props.specData : documentDate,
+          props.queryVariableLabel,
+          props.repeatStepList,
+          props.editRepeatStepListVariableLabel,
+          props.indexVariableLabel ? props.repeatStep : undefined
+        )
+      : props.label;
 
   if (props.specValueList) {
     return (
@@ -122,19 +117,41 @@ export default props => {
         label={label}
       />
     );
+  } else if (props.math || props.setValueByIndex) {
+    return (
+      <small>
+        <ReadOnlyField
+          {...props}
+          key={`${props.indexId}-${props.index}`}
+          path={getNewPath()}
+          submitButton={
+            `${props.repeatStepList}-${props.fieldName}` === editChapter
+              ? true
+              : false
+          }
+          min={min}
+          max={max}
+          label={label}
+          subtext={subtext}
+          file={props.type === "file" ? props.file : null}
+          indexId={`${props.indexId}-${props.index}`}
+          index={props.index}
+          className="mt-n3 mb-3"
+          noLine
+        />
+      </small>
+    );
   } else if (
-    props.math ||
-    props.setValueByIndex
-    
+    props.writeChapter ||
+    `${props.repeatStepList}-${props.fieldName}` === editChapter
   ) {
     return (
-      <ReadOnlyField
+      <WriteField
         {...props}
         key={`${props.indexId}-${props.index}`}
         path={getNewPath()}
         submitButton={
-          `${props.repeatStepList}-${props.fieldName}` ===
-          editChapter
+          `${props.repeatStepList}-${props.fieldName}` === editChapter
             ? true
             : false
         }
@@ -147,27 +164,7 @@ export default props => {
         index={props.index}
       />
     );
-  } else if (props.writeChapter ||
-    `${props.repeatStepList}-${props.fieldName}` === editChapter) {
-      return <WriteField 
-      {...props}
-        key={`${props.indexId}-${props.index}`}
-        path={getNewPath()}
-        submitButton={
-          `${props.repeatStepList}-${props.fieldName}` ===
-          editChapter
-            ? true
-            : false
-        }
-        min={min}
-        max={max}
-        label={label}
-        subtext={subtext}
-        file={props.type === "file" ? props.file : null}
-        indexId={`${props.indexId}-${props.index}`}
-        index={props.index}
-      />;
-    }else if (props.type === "file") {
+  } else if (props.type === "file") {
     return (
       <Input
         {...props}
@@ -186,10 +183,7 @@ export default props => {
         path={getNewPath()}
         indexId={`${props.indexId}-${props.index}`}
         index={props.index}
-        value={objectPath.get(
-          documentDate,
-          getNewPath()
-        )}
+        value={objectPath.get(documentDate, getNewPath())}
         subtext={subtext}
         label={label}
       />
