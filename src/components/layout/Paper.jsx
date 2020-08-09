4e1b5344d22ce9d847307b7eb83d658172363706
Kristian Gunder Kramås <kristiankramas@outlook.com>
2020-08-09 16:07:11 +0200

    Margin between papers

--- src/components/layout/Paper.jsx
+++ src/components/layout/Paper.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 export default ({ ...props }) => (
-  <div full={props.full ? props.full.toString() : "false"} dark={props.dark ? props.dark.toString() : "false"}>
-    <Card
-      className={`shadow ${props.dark && "bg-dark"}`}
-      style={{
-        minHeight: props.full && "100%",
-        color: props.dark && "#d5d5d5"
-      }}
+  <div {...props}>
+    <div
+      full={props.full ? props.full.toString() : "false"}
+      dark={props.dark ? props.dark.toString() : "false"}
     >
-      <Card.Body className="h-100">{props.children}</Card.Body>
-    </Card>
+      <Card
+        className={`shadow ${props.dark && "bg-dark"}`}
+        style={{
+          minHeight: props.full && "100%",
+          color: props.dark && "#d5d5d5"
+        }}
+      >
+        <Card.Body className="h-100">{props.children}</Card.Body>
+      </Card>
+    </div>
   </div>
 );
