700150660fda8b3f06abc35c64cc6c1056901358
Kristian Gunder Kramås <kristiankramas@outlook.com>
2020-10-31 17:30:01 +0100

    Repair dynamic stages

--- src/components/repair/Repair.jsx
+++ src/components/repair/Repair.jsx
@@ -1,33 +1,79 @@
 import React from "react";
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 import mutation from "graphql/mutation.js";
 import { Modal } from "react-bootstrap";
 import DepthButtonGroup from "components/button/DepthButtonGroup";
 import DepthButton from "components/button/DepthButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { camelCaseToNormal } from "functions/general";
+import { camelCaseToNormal, objectifyQuery } from "functions/general";
 import Loading from "components/Loading";
 import stages from "components/form/stage/stages.json";
 import objectPath from "object-path";
+import { useParams } from "react-router-dom";
+import gql from "graphql-tag";
 
-const repairStages = [
-  { stage: "steelPreparation1", label: "Steel Preparation 1" },
-  { stage: "steelPreparation2", label: "Steel Preparation 2" },
-  { stage: "primer1", label: "Primer" },
-  { stage: "coatingStep", label: "Coating" },
-  { stage: "touchUp", label: "Touch Up" },
-  { stage: "qualityControlCoatedItem", label: "Quality Control" }
-];
+// const repairStages = [
+//   { stage: "steelPreparation1", label: "Steel Preparation 1" },
+//   { stage: "steelPreparation2", label: "Steel Preparation 2" },
+//   { stage: "primer1", label: "Primer" },
+//   { stage: "coatingStep", label: "Coating" },
+//   { stage: "touchUp", label: "Touch Up" },
+//   { stage: "qualityControlCoatedItem", label: "Quality Control" }
+// ];
 
-function hasItemStage(leadData, stage, packerOrCoating) {
-  return !!objectPath.get(
-    leadData,
-    stages[packerOrCoating][stage]["queryPath"],
-    false
-  );
+function itemHasStage(leadData, stage, packerOrCoating) {
+  if (!!stages[packerOrCoating][stage]) {
+    return !!objectPath.get(
+      leadData,
+      stages[packerOrCoating][stage].queryPath,
+      false
+    );
+  }
+  return false;
 }
 
+const queries = {
+  item: gql`
+    query($id: Int) {
+      items(id: $id) {
+        id
+        leadEngineer {
+          id
+          data
+        }
+      }
+    }
+  `
+};
+
 export default ({ id, show, setShow, children }) => {
+  const params = useParams();
+
+  const itemQuery = useQuery(queries.item, {
+    variables: {
+      id: params.itemId
+    }
+  });
+
+  console.log(objectifyQuery(itemQuery.data));
+
+  const itemData = objectifyQuery(itemQuery.data);
+  const leadData = itemData && objectifyQuery(itemData).items[0].leadEngineer;
+  const packerOrCoating = "coating";
+
+  const repairStages = [];
+
+  stages.all.forEach(stage => {
+    if (
+      leadData &&
+      stage &&
+      packerOrCoating &&
+      itemHasStage(leadData, stage, packerOrCoating)
+    ) {
+      repairStages.push({ stage, label: camelCaseToNormal(stage) });
+    }
+  });
+
   const handleClick = (e, repairStage) => {
     e.preventDefault();
     mutationHandle({
